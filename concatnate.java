8ef2acbd9387fa0f5dc6a6b30662361a11f9e043
Hritik <73677045+Hritik003@users.noreply.github.com>
2021-07-13 12:07:37 +0530

    Add files via upload

---
+++ concatnate.java
@@ -0,0 +1,10 @@
+public class main{
+    public static void main(String[] args) {
+        String s1="Java";
+        String s2="Programming";
+        System.out.println(s1  + s2); // JavaProgramming
+        System.out.println(s1 + " " + s2);// Java Programming
+
+
+    }
+}
