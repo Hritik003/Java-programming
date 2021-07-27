dc3c2933fc10c3f26c04228026d3a48fc4257b33
Hritik <hritikraj_03@outlook.com>
2021-07-27 16:30:08 +0530

    volume of a box

---
+++ prac1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class prac1 {
+public static void main(String args[])
+                         {
+	Scanner input = new Scanner(System.in);
+	int n = input.nextInt();
+	
+	if(n%2==0)
+	{
+		System.out.println("even");
+		
+	}
+	else
+	{
+		System.out.print("odd");
+	}
+}
+}
