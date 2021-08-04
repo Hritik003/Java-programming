ffdfcdb35600349bfe34f826bca972f8e0c7dd9b
Hritik <73677045+Hritik003@users.noreply.github.com>
2021-08-04 23:47:57 +0530

    pop and peek

---
+++ usingStacks.java
@@ -0,0 +1,18 @@
+
+
+public class usingStacks {
+ public static void main(String[] args) {
+	 //DeathNOte
+	 //Money heist
+	 //Lupin
+	 java.util.Stack<String> mystack = new java.util.Stack<String>();
+   
+   mystack.add("Death Note");
+   mystack.add("Money heist");
+   mystack.add("Lupin");
+   
+   System.out.println(mystack.pop());
+   System.out.println(mystack);
+ }
+ 
+}
