ec5acdb3f53b89cfd5e4e0cba4f816fef1f254e3
Hritik <73677045+Hritik003@users.noreply.github.com>
2021-08-13 23:30:10 +0530

    Add files via upload

---
+++ ArrayListFun.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+//Array lists live in the java.util package//
+
+
+public class ArrayListFun {
+public static void main(String[] args) {
+	ArrayFUN Namelist = new ArrayFUN();
+	
+	Namelist.add("Hritik");
+	
+	
+}
+
+
+
+
+}
