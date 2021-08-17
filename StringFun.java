acc40c30f75fdf00152d9c2d5ff0f76d1111aacc
Hritik <73677045+Hritik003@users.noreply.github.com>
2021-08-17 17:46:44 +0530

    using the "equal"

---
+++ StringFun.java
@@ -0,0 +1,22 @@
+
+public class StringFun {
+public static void main(String[] args) {
+	String name = "john";
+	String name2 ="helsberg";
+	String name3 = "ben";
+	
+	if( name.equals(name3))
+	{
+		System.out.println("name and name3 are equal");
+		
+	}
+	else if( name.equals(name2))
+	{
+		System.out.println("name and name2 are equal");
+	}
+	else
+	{
+		System.out.println("all are unique");
+	}
+}
+}
