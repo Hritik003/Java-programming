93ecc023c62e11b0be00a21d3301e5c03260d06c
Hritik <73677045+Hritik003@users.noreply.github.com>
2021-08-18 17:02:30 +0530

    Substring function

---
+++ Substring.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class Substring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String S2 = S.substring(start,end);
+        System.out.println(S2);
+        in.close();
+    }
+}
