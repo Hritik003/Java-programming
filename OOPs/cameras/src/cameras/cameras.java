daf02cd74312a1c436e85a00dd3fe3ec70a0bbb8
Hritik <73677045+Hritik003@users.noreply.github.com>
2021-08-24 22:24:02 +0530

    accessing enum from camera class

--- OOPs/cameras/src/cameras/cameras.java
+++ OOPs/cameras/src/cameras/cameras.java
@@ -1,9 +1,13 @@
 package cameras;
 
+import cameras.camera.Cameratype;
+
 public class cameras {
 public static void main(String[] args) {
-	 camera camera  = new camera();
-	 camera.Serialnumber = "1356";
+	 camera camera  = new camera("13156");
+	 camera.setcameratype("EPTZ");
+	 
+	 camera.cameratype = Cameratype.EPTZ;
 	 Position Position = new Position();
 	 Position.pan = 5;
 	 Position.tilt = 10;
@@ -12,5 +16,6 @@ public static void main(String[] args) {
 	 camera.SetPosition(Position);
 	 
 	 System.out.println(camera);
+	 
 }
 }
